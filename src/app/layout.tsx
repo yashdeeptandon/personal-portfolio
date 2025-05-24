043ba0c44fddcc302042cd400df0e9dbf55fe1fb
Yashdeep Tandon <yashdeeptandon007@gmail.com>
2025-05-24 15:56:16 +0530

    Google analytics integrated

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import GoogleAnalytics from "@/components/GoogleAnalytics";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -37,6 +38,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        {/* Google Analytics */}
+        <GoogleAnalytics gaId={process.env.GOOGLE_ANALYTICS_ID || ""} />
+
         {children}
       </body>
     </html>
